15bb48f925a26f62c587edfa441ddcf1ef260d45
Devin Gallis <devingallis@outlook.com>
2018-04-08 17:33:40 +0000

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about' , to: 'pages#about'
-    get 'new' , to: 'pages#new'
+    get 'north' , to: 'pages#north'
+    get 'south' , to: 'pages#south'
+    get 'east' , to: 'pages#east'
+    get 'west' , to: 'pages#west'
 end
